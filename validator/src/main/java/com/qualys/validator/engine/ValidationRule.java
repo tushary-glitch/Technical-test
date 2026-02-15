567eabb332a4670c107856673f1a84b044781410
Tushar Yeole <tushar.y@gmail.com>
2026-02-15 21:14:19 +0530

    created new branch

--- validator/src/main/java/com/qualys/validator/engine/ValidationRule.java
+++
@@ -1,7 +0,0 @@
-package com.qualys.validator.engine;
-
-import com.qualys.validator.model.Record;
-
-public interface ValidationRule {
-    void validate(Record record, ValidationContext context, ValidationResult result);
-}
--- validator/src/main/java/com/qualys/validator/model/Metadata.java
+++
@@ -1,8 +0,0 @@
-package com.qualys.validator.model;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public class Metadata {
-    @JsonProperty("channel")
-    public String channel;
-}
